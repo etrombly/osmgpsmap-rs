8a97a3fe3ec2cff52cbf46a97146b1127cc91b65
Eric Trombly <etrombly@yahoo.com>
2018-04-24 04:31:24 -0500

    updates to Gir.toml

--- src/auto/mod.rs
+++ src/auto/mod.rs
@@ -2,11 +2,39 @@
 // from gir-files (https://github.com/gtk-rs/gir-files @ ???)
 // DO NOT EDIT
 
-mod constants;
-pub use self::constants::MAP_CACHE_AUTO;
-pub use self::constants::MAP_CACHE_DISABLED;
-pub use self::constants::MAP_CACHE_FRIENDLY;
+mod map;
+pub use self::map::Map;
+pub use self::map::MapExt;
+
+mod map_image;
+pub use self::map_image::MapImage;
+pub use self::map_image::MapImageExt;
+
+mod map_layer;
+pub use self::map_layer::MapLayer;
+pub use self::map_layer::MapLayerExt;
+
+mod map_osd;
+pub use self::map_osd::MapOsd;
+pub use self::map_osd::MapOsdExt;
+
+mod map_polygon;
+pub use self::map_polygon::MapPolygon;
+pub use self::map_polygon::MapPolygonExt;
+
+mod map_track;
+pub use self::map_track::MapTrack;
+pub use self::map_track::MapTrackExt;
+
+mod map_point;
+pub use self::map_point::MapPoint;
 
 #[doc(hidden)]
 pub mod traits {
+    pub use super::MapExt;
+    pub use super::MapImageExt;
+    pub use super::MapLayerExt;
+    pub use super::MapOsdExt;
+    pub use super::MapPolygonExt;
+    pub use super::MapTrackExt;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+extern crate osm_gps_map_sys as ffi;
+extern crate gdk;
+extern crate gdk_pixbuf;
+extern crate glib;
+extern crate glib_sys as glib_ffi;
+extern crate gobject_sys as gobject_ffi;
+extern crate gtk;
+extern crate gtk_sys as gtk_ffi;
+extern crate cairo;
+extern crate libc;
+
+mod auto;
+
+pub use auto::*;
